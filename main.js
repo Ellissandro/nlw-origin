b2f75f684bb83d6bcb359ebcbbd09d82d8fc030a
Elissandro Silva <elissandro.soares@escolaweb.com.br>
2021-06-23 20:39:19 -0300

    added animation

--- main.js
+++ main.js
@@ -1,27 +1,53 @@
-const nav = document.querySelector('#header nav');
+const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
-    nav.classList.toggle('show');
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
   })
 }
 
-const links = document.querySelectorAll('nav ul li a');
+const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function() {
-    nav.classList.remove('show');
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
 
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
-    header.classList.add('scroll');
+    header.classList.add('scroll')
   } else {
-    header.classList.remove('scroll');
+    header.classList.remove('scroll')
   }
 })
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
